694cd59774abd83976e8ee5e06d5f503cd611ce6
hwsiew <hw.siew.work@gmail.com>
2021-08-08 12:48:25 +0800

    Add tailwind theme style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#7faae4',
+        'secondary': '#834db7',
+        'tertiary': '#34283f'
+      },
+      fontSize: {
+        'heading': '18px',
+        'body': '16px'
+      }
+    },
   },
   variants: {
     extend: {},
