d32d9607563ffeedabba6fc76ff8d4e0f05c4320
hwsiew <hw.siew.work@gmail.com>
2021-08-08 17:20:27 +0800

    Add simple test for App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { 
+  QueryClient,
+  QueryClientProvider
+} from 'react-query';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const queryClient = new QueryClient();
+
+it('renders appbar with logo', () => {
+  render(
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  );
+  const logoElement1 = screen.getByText(/hey/i);
+  const logoElement2 = screen.getByText(/cinema/i);
+  expect(logoElement1).toBeInTheDocument();
+  expect(logoElement2).toBeInTheDocument();
 });
